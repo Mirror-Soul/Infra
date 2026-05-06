fd9cbd713e0c5b71b25e066c0666a14d2030e934
ljj5616 <ljj10571@gmail.com>
2026-05-07 01:30:29 +0900

    add network in main vpc

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,39 @@ resource "aws_vpc" "main" {
         Name = "mirrorsoul-vpc"
     }
 }
+
+resource "aws_subnet" "public_a" {
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = "10.0.1.0/24"
+    availability_zone       = "ap-northeast-2a"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "mirrorsoul-public-subnet-a"
+    } 
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "mirrorsoul-igw"
+    }
+}
+
+resource "aws_route_table" "rt" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "mirrorsoul-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "rta" {
+    subnet_id = aws_subnet.public_a.id
+    route_table = aws_route_table.rt.id
+}
