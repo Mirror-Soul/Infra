b5165947735c796e72613814581ef120ac88c776
ljj5616 <ljj10571@gmail.com>
2026-05-07 02:32:47 +0900

    create ec2 for api server

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ data "aws_ami" "amazon_linux_2023" {
 
 resource "aws_instance" "api_server" {
     ami                     = data.aws_ami.amazon_linux_2023.id
-    instance_type           = "t3.micro"'
+    instance_type           = "t3.micro"
     subnet_id               = aws_subnet.public_a.id
     vpc_security_group_ids  = [aws_security_group.sg_api_server.id]
     key_name                = "mirrorsoul-api-key"
