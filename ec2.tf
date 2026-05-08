60cb457a4a0391fcf1c7ec0bd772ddd7865fbec1
ljj5616 <ljj10571@gmail.com>
2026-05-09 01:51:07 +0900

    fix ami version

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,5 @@
-data "aws_ami" "amazon_linux_2023" {
-    most_recent = true
-    owners = ["amazon"]
-
-    filter {
-        name = "name"
-        values = ["al2023-ami-*-x86_64"]
-    }
-}
-
 resource "aws_instance" "api_server" {
-    ami                     = data.aws_ami.amazon_linux_2023.id
+    ami                     = "ami-01cbcf53c1503fe8a"
     instance_type           = "t3.micro"
     subnet_id               = aws_subnet.public_a.id
     vpc_security_group_ids  = [aws_security_group.sg_api_server.id]
