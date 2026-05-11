764b91088228cfa25e577cb1bb0ef5c4153a87a2
ljj5616 <ljj10571@gmail.com>
2026-05-11 23:04:36 +0900

    allow ec2 http & https

--- security-group.tf
+++ security-group.tf
@@ -23,6 +23,26 @@ resource "aws_security_group" "sg_api_server" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        description = "HTTP"
+
+        from_port = 80
+        to_port   = 80
+        protocol  = "tcp"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTPS"
+
+        from_port = 443
+        to_port   = 443
+        protocol  = "tcp"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         description = "Allow all outbound"
 
