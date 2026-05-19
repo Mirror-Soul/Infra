c7092a39bfa138e3425c9c77e7a70b52c7839c3f
ljj5616 <ljj10571@gmail.com>
2026-05-19 23:52:48 +0900

    create security group and ec2 for AI server

--- ec2.tf
+++ ec2.tf
@@ -16,3 +16,22 @@ resource "aws_instance" "api_server" {
         Name = "mirrorsoul-api-server"
     }
 }
+
+resource "aws_instance" "ai_server" {
+    ami                     = "ami-01cbcf53c1503fe8a"
+    instance_type           = "t3.small"
+    subnet_id               = aws_subnet.public_a.id
+    vpc_security_group_ids  = [aws_security_group.sg_ai_server.id]
+    key_name                = "mirrorsoul-ai-key"
+
+    iam_instance_profile = aws_iam_instance_profile.api_server_profile.name     # 나중에 분리해줘야 함
+
+    root_block_device {
+        volume_size = 20
+        volume_type = "gp3"
+    }
+
+    tags = {
+        Name = "mirrorsoul-ai-server"
+    }
+}
--- security-group.tf
+++ security-group.tf
@@ -57,3 +57,44 @@ resource "aws_security_group" "sg_api_server" {
         Name = "mirrorsoul-api-sg"
     }
 }
+
+resource "aws_security_group" "sg_ai_server" {
+    name = "mirrorsoul-ai-sg"
+    description = "Security group for AI server"
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        description = "SSH"
+
+        from_port = 22
+        to_port   = 22
+        protocol  = "tcp"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # 굳이 안열어도 되지만 swagger나 그런거 사용 위해 열어둠
+    ingress {
+        description = "FastAPI"
+
+        from_port   = 8000
+        to_port     = 8000
+        protocol    = "tcp"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "Allow all outbound"
+
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "mirrorsoul-ai-sg"
+    }
+}
