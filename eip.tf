9c1056ea8bc448c2e0987afa228ee8a54a6ddf64
ljj5616 <ljj10571@gmail.com>
2026-05-22 01:20:17 +0900

    rename eip.tf

--- eip.tf
+++ eip.tf
@@ -10,3 +10,16 @@ resource "aws_eip_association" "api_server_eip_assoc" {
   instance_id   = aws_instance.api_server.id
   allocation_id = aws_eip.api_server_eip.id
 }
+
+resource "aws_eip" "ai_server_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "mirrorsoul-ai-server-eip"
+  }
+}
+
+resource "aws_eip_association" "ai_server_eip_asspc" {
+  instance_id   = aws_instance.ai_server.id
+  allocation_id = aws_eip.ai_server_eip.id
+}
