21ec4533bd79f9538e850cbfa0d026cd0d4d50f1
ljj5616 <ljj10571@gmail.com>
2026-05-22 01:51:36 +0900

    create ai server IAM ROLE & policy + api server SQS policy

--- ec2.tf
+++ ec2.tf
@@ -1,37 +1,37 @@
 resource "aws_instance" "api_server" {
-    ami                     = "ami-01cbcf53c1503fe8a"
-    instance_type           = "t3.micro"
-    subnet_id               = aws_subnet.public_a.id
-    vpc_security_group_ids  = [aws_security_group.sg_api_server.id]
-    key_name                = "mirrorsoul-api-key"
+  ami                    = "ami-01cbcf53c1503fe8a"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public_a.id
+  vpc_security_group_ids = [aws_security_group.sg_api_server.id]
+  key_name               = "mirrorsoul-api-key"
 
-    iam_instance_profile = aws_iam_instance_profile.api_server_profile.name
+  iam_instance_profile = aws_iam_instance_profile.api_server_profile.name
 
-    root_block_device {
-        volume_size = 10
-        volume_type = "gp3"
-    }
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+  }
 
-    tags = {
-        Name = "mirrorsoul-api-server"
-    }
+  tags = {
+    Name = "mirrorsoul-api-server"
+  }
 }
 
 resource "aws_instance" "ai_server" {
-    ami                     = "ami-01cbcf53c1503fe8a"
-    instance_type           = "t3.small"
-    subnet_id               = aws_subnet.public_a.id
-    vpc_security_group_ids  = [aws_security_group.sg_ai_server.id]
-    key_name                = "mirrorsoul-ai-key"
+  ami                    = "ami-01cbcf53c1503fe8a"
+  instance_type          = "t3.small"
+  subnet_id              = aws_subnet.public_a.id
+  vpc_security_group_ids = [aws_security_group.sg_ai_server.id]
+  key_name               = "mirrorsoul-ai-key"
 
-    iam_instance_profile = aws_iam_instance_profile.api_server_profile.name     # 나중에 분리해줘야 함
+  iam_instance_profile = aws_iam_instance_profile.ai_server_profile.name # 나중에 분리해줘야 함
 
-    root_block_device {
-        volume_size = 20
-        volume_type = "gp3"
-    }
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
 
-    tags = {
-        Name = "mirrorsoul-ai-server"
-    }
+  tags = {
+    Name = "mirrorsoul-ai-server"
+  }
 }
--- iam.tf
+++ iam.tf
@@ -1,7 +1,8 @@
+# API 서버 IAM ROLE
 resource "aws_iam_role" "api_server_role" {
-    name = "mirrorsoul-api-server-role"
+  name = "mirrorsoul-api-server-role"
 
-    assume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
 
     Statement = [
@@ -18,6 +19,7 @@ resource "aws_iam_role" "api_server_role" {
   })
 }
 
+# API 서버 S3 정책
 resource "aws_iam_role_policy" "s3_policy" {
   name = "mirrorsoul-s3-policy"
   role = aws_iam_role.api_server_role.id
@@ -54,7 +56,109 @@ resource "aws_iam_role_policy" "s3_policy" {
   })
 }
 
+# API 서버 SQS 정책
+resource "aws_iam_role_policy" "api_sqs_policy" {
+  name = "mirrorsoul-api-sqs-policy"
+  role = aws_iam_role.api_server_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "sqs:SendMessage",
+          "sqs:GetQueueUrl",
+          "sqs:GetQueueAttributes"
+        ]
+
+        Resource = aws_sqs_queue.ai_job_queue.arn
+      }
+    ]
+  })
+}
+
 resource "aws_iam_instance_profile" "api_server_profile" {
   name = "mirrorsoul-api-server-profile"
   role = aws_iam_role.api_server_role.name
+}
+
+#---------------------------------AI------------------------------------#
+
+# AI 서버 IAM ROLE 설정
+resource "aws_iam_role" "ai_server_role" {
+  name = "mirrorsoul-ai-server-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# AI 서버 S3, SQS 관련 IAM policy 설정
+resource "aws_iam_role_policy" "ai_s3_sqs_policy" {
+  name = "mirrorsoul-ai-s3-sqs-policy"
+  role = aws_iam_role.ai_server_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+
+        Resource = [
+          "${aws_s3_bucket.storage.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:ListBucket"
+        ]
+
+        Resource = [
+          aws_s3_bucket.storage.arn
+        ]
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueUrl",
+          "sqs:GetQueueAttributes",
+          "sqs:ChangeMessageVisibility"
+        ]
+
+        Resource = aws_sqs_queue.ai_job_queue.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ai_server_profile" {
+  name = "mirrorsoul-ai-server-profile"
+  role = aws_iam_role.ai_server_role.name
 }
