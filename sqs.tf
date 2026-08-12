98cf1dd932d685aa553f1c73c0d9956017b60a3e
이정준 <ljj10571@gmail.com>
2026-08-12 13:37:18 +0900

    create face training sqs queue

--- sqs.tf
+++ sqs.tf
@@ -6,3 +6,12 @@ resource "aws_sqs_queue" "ai_job_queue" {
   message_retention_seconds  = 345600 # 메세지 최대 4일 보관
   receive_wait_time_seconds  = 20     # long polling
 }
+
+# 얼굴 가공 작업 요청 용 SQS 생성
+resource "aws_sqs_queue" "face_training_queue" {
+  name = "mirrorsoul-face-training-queue"
+
+  visibility_timeout_seconds = 3600
+  message_retention_seconds = 345600
+  receive_wait_time_seconds = 20
+}
