4c9138347d7691c7cfe5987776425813779c7588
이정준 <ljj10571@gmail.com>
2026-08-18 14:48:59 +0900

    add authority to see sqs & s3 to kim

--- iam-user.tf
+++ iam-user.tf
@@ -45,3 +45,61 @@ resource "aws_iam_group_policy_attachment" "attach_ec2_start_stop_policy" {
   group      = aws_iam_group.ec2_start_stop_group.name
   policy_arn = aws_iam_policy.ec2_start_stop_policy.arn
 }
+
+# kim 사용자가 S3 버킷/객체와 SQS 큐/메시지를 조회할 수 있는 읽기 전용 정책
+resource "aws_iam_user_policy" "kim_storage_queue_read_only" {
+  name = "mirrorsoul-storage-queue-read-only"
+  user = aws_iam_user.team_ec2_ops["kim"].name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ListS3Buckets"
+        Effect = "Allow"
+        Action = [
+          "s3:ListAllMyBuckets"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ReadStorageBucket"
+        Effect = "Allow"
+        Action = [
+          "s3:GetBucketLocation",
+          "s3:ListBucket"
+        ]
+        Resource = aws_s3_bucket.storage.arn
+      },
+      {
+        Sid    = "ReadStorageObjects"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = "${aws_s3_bucket.storage.arn}/*"
+      },
+      {
+        Sid    = "ListSqsQueues"
+        Effect = "Allow"
+        Action = [
+          "sqs:ListQueues"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ReadQueueMessages"
+        Effect = "Allow"
+        Action = [
+          "sqs:GetQueueAttributes",
+          "sqs:GetQueueUrl",
+          "sqs:ReceiveMessage"
+        ]
+        Resource = [
+          aws_sqs_queue.ai_job_queue.arn,
+          aws_sqs_queue.face_training_queue.arn
+        ]
+      }
+    ]
+  })
+}
